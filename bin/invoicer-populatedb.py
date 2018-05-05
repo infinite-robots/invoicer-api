a48fea070bb6408653076d6140bd858b268ed2b6
Caden <cadengrey@icloud.com>
2018-05-05 16:55:44 -0400

    Updated population script.

--- bin/invoicer-populatedb.py
+++ bin/invoicer-populatedb.py
@@ -1,8 +1,11 @@
+from invoicer.application import User
 from invoicer.application import db
 
 def main():
     try:
-        db.create_all()
+        dave = User(username='dave', email='dave@example.com')
+        db.session.add(dave)
+        db.session.commit()
     except Exception, e:
         if u'Unknown database' in e.message:
             print "ERROR: Please create 'invoicer' database"
